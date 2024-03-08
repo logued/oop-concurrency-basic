4163e9785db3917e080f09f8ca128db045726f01
dermo <dermot.logue@dkit.ie>
2024-03-08 23:27:01 +0000

    use 2 threads but 3 runnables - threads will be reused.

--- src/main/java/org/example/sample4/SumAllMain.java
+++ src/main/java/org/example/sample4/SumAllMain.java
@@ -16,24 +16,31 @@ public class SumAllMain {
         listOfRunnables.add(new SumRange_Runnable(11, 20));
         listOfRunnables.add(new SumRange_Runnable(21, 30));
 
-        final int total_threads = 3;    // number of threads in the thread pool
+        final int number_of_threads = 2;    // number of threads in the thread pool
 
+        // Note that we have two threads in the thread pool, but we queue up 3 runnable tasks
+        // to be executed. The first two will be executed, and when one finishes, the third will be
+        // run on the thread that has become idle.
         System.out.println("Assigning three runnables to the Executor thread-pool.");
-        ExecutorService executor = Executors.newFixedThreadPool(total_threads);
-        for (int i = 0; i < total_threads; i++) {
+        ExecutorService executor = Executors.newFixedThreadPool(number_of_threads);
+        for (int i = 0; i < listOfRunnables.size(); i++) {      // queue up all 3 runnables to be executed in thread pool
             executor.execute(listOfRunnables.get(i));
         }
-
-        // If we want to WAIT for the runnables to complete we need the following code
 
         int count = 0;
 
         executor.shutdown(); // request that the thread pool be shut down (not instantaneous)
+        // AAfter shutdown, the executor will not accept any more runnable tasks to be executed.
 
-        // wait for all threads in the pool to have finished executing their runnables
-        // before we call the getSum() methods in the runnable tasks.
-        // This serves teh same purpose of the thread.join() in the previous samples
-        // It stops the main thread here until all executor threads in pool have terminated.
+        // The runnable tasks will be executed in the thread pool (one runnable per thread).
+        // We must pause the main thread (that main() is running on ) until all the
+        // runnables have completed their work.
+        // The code below uses awaitTermination() to achieve this.
+        // This serves the same purpose of the thread.join() in the previous samples -
+        // it stops the main thread here until all queued executor runnable tasks in pool have terminated.
+        // Once all have finished, the main thread can proceed and the 'sum' results will
+        // be available in the runnable objects. We can then add them all up.
+
         try {
             while (!executor.awaitTermination(100, TimeUnit.MILLISECONDS)) {
                 // if all threads have not terminated in the last 100ms interval, then
@@ -48,7 +55,9 @@ public class SumAllMain {
         System.out.println("Executor is shutdown, and all threads have finished their work....");
         System.out.println("Summing up the 'sum' totals for all the runnables....");
         int total = 0;
+        int i=0;
         for (SumRange_Runnable r : listOfRunnables) {
+            System.out.println("Sum from runnable " + ++i +" = " + r.getSum() );
             total = total + r.getSum();
         }
         System.out.println("Grand total = " + total);
